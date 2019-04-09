14687a17e9131b7a2f7ded49d6df8dd27b695de3
Azat <46015824+serikovskii@users.noreply.github.com>
2019-04-09 10:11:38 +0600

    теория

---
+++ AutoLevelPart2/AutoLevelPart2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+
+namespace AutoLevelPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var providerName = ConfigurationManager.ConnectionStrings["appConnection"].ProviderName; 
+            var connectionString = ConfigurationManager.ConnectionStrings["appConnection"].ConnectionString;
+
+            var factory = DbProviderFactories.GetFactory(providerName);
+            var connection = factory.CreateConnection();
+            var command = connection.CreateCommand();
+
+            connection.ConnectionString = connectionString;
+            command.CommandText = "select * from Users";
+
+            var dataSet = new DataSet("usersApp");
+            var dataAdapter = factory.CreateDataAdapter();
+
+            dataAdapter.SelectCommand = command;
+
+            var commandBuilder = factory.CreateCommandBuilder();
+            commandBuilder.DataAdapter = dataAdapter;
+
+            connection.Open();
+            dataAdapter.Fill(dataSet, "Users");
+            connection.Close();
+            //commandBuilder.Dispose();
+
+            var firstRow = dataSet.Tables["Users"].Rows[0];
+
+            var user = new User
+            {
+                Id = (int)firstRow["Id"],
+                Login = firstRow["Login"].ToString(),
+                Password = firstRow["Password"].ToString()
+            };
+
+            firstRow.BeginEdit();
+            firstRow.ItemArray = new object[] {user.Id, "asd", "dsa" };
+            firstRow.EndEdit();
+
+            dataAdapter.Update(dataSet, "Users");
+        }
+    }
+}
---
+++ AutoLevelPart2/AutoLevelPart2/User.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoLevelPart2
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
